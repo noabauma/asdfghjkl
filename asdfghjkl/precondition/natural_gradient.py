41eb5f852358406962e637f282d6fec735671e11
Noah Baumann <noabauma@student.ethz.ch>
2022-12-06 17:37:52 +0100

    reduce_scatter kfac test

--- asdfghjkl/precondition/natural_gradient.py
+++ asdfghjkl/precondition/natural_gradient.py
@@ -486,8 +486,6 @@ class NaturalGradientMaker(PreconditionedGradientMaker):
         rank = 0
         tensor_list = []
         for enum_shape, shape in enumerate(_module_level_shapes):
-            if enum_shape == 4:
-                continue
             keys_list = self._keys_list_from_shape(shape)
             for enum_module, name_module in enumerate(self.named_modules_for(shape)):
 
